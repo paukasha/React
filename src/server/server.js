fbf2295bf29bad809690955d4c1b36b1e51eb946
paukasha <odinpozitivchik@mail.ru>
2022-09-17 19:30:31 +1000

    промежуточный

--- src/server/server.js
+++ src/server/server.js
@@ -14,16 +14,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/auth', (req, res) => {
-  console.log('code',req.query.code)
   axios.post(
     'https://www.reddit.com/api/v1/access_token',
     `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
     {
-      auth: { username: process.env.CLIENT_ID, password: 'znd-6OHLzgvP3c4yHUjI5kaVuadZUg'},
-      headers: { 'Content-type': 'application/x-www-form-urlencoded'}
+      auth: {
+        username: process.env.CLIENT_ID,
+        password: 'dFDinx05ivnpw-EG6IQoR69GgG0k9w'
+      },
+      headers: { 'Content-type': 'application/x-www-form-urlencoded',
+      }
     })
     .then(({ data }) => {
-      console.log(data)
       res.send(
         indexTemplate(ReactDOM.renderToString(App()), data['access_token']),
       );
